466298ebfe728435a6ccb355c83bb1b57a2177ea
Vincent Arlou <arlou@protonmail.com>
2021-11-14 13:26:24 -0500

    possible fix

--- app/pages/api/d/projects.js
+++ app/pages/api/d/projects.js
@@ -1,4 +1,4 @@
-import { projects } from "../../../__mocks__/projects";
+import projects from "../../../__mocks__/projects";
 
 export default async (req, res) => {
   res.status(200).json(projects);
