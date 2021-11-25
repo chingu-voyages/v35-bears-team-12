e62daa50c9605616550c7729b5a0d8da70ccbcfc
Alex Reitz <alex.j.reitz@gmail.com>
2021-11-25 11:03:16 -0500

    package-lock.json

--- app/pages/about.js
+++ app/pages/about.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { ContentWrapper } from "../layouts/content-wrapper";
 import { Container } from "../layouts/container";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 
 export default function About() {
   return (
     <Container title="About | Chingu Board">
-      <ContentWrapper>
-        <Heading as="h2" textAlign="center" variant="h2">
-          About Us
-        </Heading>
-      </ContentWrapper>
+      <Heading as="h2" textAlign="center" variant="h2">
+        About Us
+      </Heading>
+      <Text>
+        Turn what you've learned online, in bootcamps, and in school into the
+        experience you need to land a software developer job. Level up your
+        technical and soft skills sought after by employers.
+      </Text>
     </Container>
   );
 }
